192c4e72f5a2f883bc722b526005c18e6adfa1f3
Hiromi Acosta Quevedo <mafequevedo1@gmail.com>
2022-09-20 19:44:55 -0500

    Ejercicios array

---
+++ corte 2/ejercicios en clase/sept20/ejercicio1/script.js
@@ -0,0 +1,80 @@
+let animales = ["Abeja", "Burro", "Camaleón", "Dodo", "Elefante"];
+
+let pais = [
+    {
+        Nombre: "Japón",
+    },
+    {
+        Capital: "Tokio",
+    },
+    {
+        Kilometros_cuadrados: 378.0,
+    },
+    {
+        Idioma: "Japonés",
+    },
+    {
+        PIB_millonesEuros: 990290,
+    },
+];
+
+let artistas = [
+    {
+        Nombre: "BlackPink",
+        Nacionalidad: "Corea del Sur",
+        Genero_musical: "Kpop",
+        canciones: [
+            {
+                cancion: "Pink Venom",
+                reproducciones_millones: 90.4,
+            },
+            {
+                cancion: "How you like that",
+                reproducciones_millones: 82.3,
+            },
+            {
+                cancion: "Ice Cream",
+                reproducciones_millones: 79,
+            },
+        ],
+    },
+    {
+        Nombre: "Kygo",
+        Nacionalidad: "Noruega",
+        Genero_musical: "Dance/electrónica",
+        canciones: [
+            {
+                cancion1: "Lost without you",
+                reproducciones_millones: 9.9,
+            },
+            {
+                cancion1: "Think about you",
+                reproducciones_millones: 51,
+            },
+            {
+                cancion1: "never really loved me",
+                reproducciones_millones: 2.6,
+            },
+        ],
+    },
+    {
+        Nombre: "Efecto pasillo",
+        Nacionalidad: "España",
+        Genero_musical: "Rock/Pop en español",
+        canciones: [
+            {
+                cancion1: "Pan y mantequilla",
+                reproducciones_millones: 55,
+            },
+            {
+                cancion1: "Carita de buena",
+                reproducciones_millones: 5,
+            },
+            {
+                cancion1: "Como tú",
+                reproducciones_millones: 2.9,
+            },
+        ],
+    },
+];
+console.log(artistas);
