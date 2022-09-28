5161232f145b63a268486244ff522465f1079871
Hiromi Acosta Quevedo <mafequevedo1@gmail.com>
2022-09-27 21:01:32 -0500

    funciones

---
+++ corte 2/ejercicios en clase/sep27/script.js
@@ -0,0 +1,5 @@
+function valores(estatura, peso) {
+    let peso = document.querySelector("#peso").value;
+    let estatura = document.querySelector("#estatura").value;
+    contenedor.innerHTML = mayusculas(texto);
+}
